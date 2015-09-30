0ac590a38cd9f3c35442d635cd6e8381f8d1e419
Arnar Freyr Bjarnason <arnarb12@ru.is>
2015-09-30 12:09:24 +0000

    Parsing port number

--- src/httpd.c
+++ src/httpd.c
@@ -15,78 +15,85 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-        int sockfd;
-        struct sockaddr_in server, client;
-        char message[512];
+    int sockfd, port;
+    struct sockaddr_in server, client;
+    char message[512];
 
-        /* Create and bind a UDP socket */
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        memset(&server, 0, sizeof(server));
-        server.sin_family = AF_INET;
-        /* Network functions need arguments in network byte order instead of
-           host byte order. The macros htonl, htons convert the values, */
-        server.sin_addr.s_addr = htonl(INADDR_ANY);
-        server.sin_port = htons(32000);
-        bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+    if(argc < 2){
+        perror("1 argument needed");
+        exit(1);
+    }
+    port = (int) atoi(argv[1]);
 
-	/* Before we can accept messages, we have to listen to the port. We allow one
-	 * 1 connection to queue for simplicity.
-	 */
-	listen(sockfd, 1);
+    /* Create and bind a UDP socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    /* Network functions need arguments in network byte order instead of
+       host byte order. The macros htonl, htons convert the values, */
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(port);
+    bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
 
-        for (;;) {
-                fd_set rfds;
-                struct timeval tv;
-                int retval;
+    /* Before we can accept messages, we have to listen to the port. We allow one
+     * 1 connection to queue for simplicity.
+     */
+    listen(sockfd, 1);
 
-                /* Check whether there is data on the socket fd. */
-                FD_ZERO(&rfds);
-                FD_SET(sockfd, &rfds);
+    for (;;) {
+        fd_set rfds;
+        struct timeval tv;
+        int retval;
 
-                /* Wait for five seconds. */
-                tv.tv_sec = 5;
-                tv.tv_usec = 0;
-                retval = select(sockfd + 1, &rfds, NULL, NULL, &tv);
+        /* Check whether there is data on the socket fd. */
+        FD_ZERO(&rfds);
+        FD_SET(sockfd, &rfds);
 
-                if (retval == -1) {
-                        perror("select()");
-                } else if (retval > 0) {
-                        /* Data is available, receive it. */
-                        assert(FD_ISSET(sockfd, &rfds));
+        /* Wait for five seconds. */
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        retval = select(sockfd + 1, &rfds, NULL, NULL, &tv);
 
-                        /* Copy to len, since recvfrom may change it. */
-                        socklen_t len = (socklen_t) sizeof(client);
+        if (retval == -1) {
+            perror("select()");
+        } else if (retval > 0) {
+            /* Data is available, receive it. */
+            assert(FD_ISSET(sockfd, &rfds));
 
-                        /* For TCP connectios, we first have to accept. */
-                        int connfd;
-                        connfd = accept(sockfd, (struct sockaddr *) &client,
-                                        &len);
-                        
-                        /* Receive one byte less than declared,
-                           because it will be zero-termianted
-                           below. */
-                        ssize_t n = read(connfd, message, sizeof(message) - 1);
+            /* Copy to len, since recvfrom may change it. */
+            socklen_t len = (socklen_t) sizeof(client);
 
-                        /* Send the message back. */
-                        write(connfd, message, (size_t) n);
+            /* For TCP connectios, we first have to accept. */
+            int connfd;
+            connfd = accept(sockfd, (struct sockaddr *) &client,
+                    &len);
 
-                        /* We should close the connection. */
-                        shutdown(connfd, SHUT_RDWR);
-                        close(connfd);
+            /* Receive one byte less than declared,
+               because it will be zero-termianted
+               below. */
+            ssize_t n = read(connfd, message, sizeof(message) - 1);
 
-                        /* Zero terminate the message, otherwise
-                           printf may access memory outside of the
-                           string. */
-                        message[n] = '\0';
-                        /* Print the message to stdout and flush. */
-                        fprintf(stdout, "Received:\n%s\n", message);
-                        fflush(stdout);
-                } else {
-                        fprintf(stdout, "No message in five seconds.\n");
-                        fflush(stdout);
-                }
+            /* Send the message back. */
+            write(connfd, message, (size_t) n);
+
+            /* We should close the connection. */
+            shutdown(connfd, SHUT_RDWR);
+            close(connfd);
+
+            /* Zero terminate the message, otherwise
+               printf may access memory outside of the
+               string. */
+            message[n] = '\0';
+            /* Print the message to stdout and flush. */
+            fprintf(stdout, "Received:\n%s\n", message);
+            fflush(stdout);
+        } else {
+            fprintf(stdout, "No message in five seconds.\n");
+            fflush(stdout);
         }
+    }
 }
